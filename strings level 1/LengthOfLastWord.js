48bea97259172bb6556ad83209182ed1ef190502
Pandit Jaydeep <jaydeepiitd95@gmail.com>
2025-10-27 22:02:45 +0530

    Length of Last word using second approach added

--- strings level 1/LengthOfLastWord.js
+++ strings level 1/LengthOfLastWord.js
@@ -1,20 +1,47 @@
+// Approach - 1
+
+// concept :- At first we removed the empty spaces from start and end then we start counting the characters from last in this example we if there is any empty spaces we negatively counting the n (keep in mind It should go in negative indexes that's why we keep the the condition in while ) and increase the the count and as soon as it encountered the the empty space the break the loop and returen the count it will the count of Last word.
+
+// function LengthOfLastWord(s){
+//     let n = s.length - 1;
+//     while(n>=0){
+//         if(s[n] === " " ){
+//             --n
+//         }else{
+//             break;
+//         }
+//     }
+//     let count = 0;
+//     while(n>=0){
+//         if(s[n] != " "){          
+//             --n
+//             ++count;
+//         }else{
+//             break;
+//         }
+//     }
+//     return count;
+// }
+
+// const result = LengthOfLastWord("Hello     Jaydeep  ")
+// console.log(result)
+
+
+
+// Approach - 2
+
+// In this approach we basically increased the count if there is no space and break the loop if count has greater value then 0 because till then we already counted the characters.
+
 function LengthOfLastWord(s){
-    let n = s.length - 1;
+    let n = s.length - 1
+    let count = 0
     while(n>=0){
-        if(s[n] === " " ){
-            --n
-        }else{
+        if(s[n] != " "){
+            ++count
+        }else if(count > 0){
             break;
         }
-    }
-    let count = 0;
-    while(n>=0){
-        if(s[n] != " "){          
-            --n
-            ++count;
-        }else{
-            break;
-        }
+        --n
     }
     return count;
 }
@@ -22,5 +49,3 @@ function LengthOfLastWord(s){
 const result = LengthOfLastWord("Hello     Jaydeep  ")
 console.log(result)
 
-
-// concept :- At first we removed the empty spaces from start and end then we start counting the characters from last in this example we if there is any empty spaces we negatively counting the n (keep in mind It should go in negative indexes that's why we keep the the condition in while ) and increase the the count and as soon as it encountered the the empty space the break the loop and returen the count it will the count of Last word.
