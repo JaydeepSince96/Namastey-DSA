25ddbc5b11fe02cc1aad5803dc66f471aa78d5a3
Pandit Jaydeep <jaydeepiitd95@gmail.com>
2025-11-08 11:49:44 +0530

    Non zero concept added

--- Array level 0/MoveZeros.js
+++ Array level 0/MoveZeros.js
@@ -1,3 +1,9 @@
+// Such type of Question's pattern called Two pointer because for such question we are tracking indixes and maintain another variable to hold values for the comparison
+
+// Concepta)- It is simple first we maintain a variable with 0 values, we iterate the items of array and if there is non zero number we shift the value of non zero number to nums[x] and then incremented the the variable. Once we shifted all the nonzero numbers to the left side of the array then  we can fill the rest indexs with zero
+
+
+
 function MoveZeros(nums){
     let x =0
     for(let i=0;i<nums.length;i++){
